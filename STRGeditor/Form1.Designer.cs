f47ceb2cd45847ef0ce31cc6711268992af176f3
angel333119 <angel-3-33@hotmail.com>
2016-09-11 19:16:40 -0300

    first commit

---
+++ STRGeditor/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace STRGeditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(755, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.saveToolStripMenuItem,
+            this.salvarToolStripMenuItem,
+            this.salvarComoToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.arquivoToolStripMenuItem.Text = "File";
+            this.arquivoToolStripMenuItem.Click += new System.EventHandler(this.arquivoToolStripMenuItem_Click);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.abrirToolStripMenuItem.Text = "Open File";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(194, 6);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.saveToolStripMenuItem.Text = "Save File";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // salvarToolStripMenuItem
+            // 
+            this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.salvarToolStripMenuItem.Text = "Save File As...";
+            this.salvarToolStripMenuItem.Click += new System.EventHandler(this.salvarToolStripMenuItem_Click);
+            // 
+            // salvarComoToolStripMenuItem
+            // 
+            this.salvarComoToolStripMenuItem.Name = "salvarComoToolStripMenuItem";
+            this.salvarComoToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.salvarComoToolStripMenuItem.Text = "Save Compressed File...";
+            this.salvarComoToolStripMenuItem.Click += new System.EventHandler(this.salvarComoToolStripMenuItem_Click);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.sobreToolStripMenuItem.Text = "About";
+            this.sobreToolStripMenuItem.Click += new System.EventHandler(this.sobreToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(755, 432);
+            this.splitContainer1.SplitterDistance = 86;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(86, 432);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.text});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(665, 432);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // text
+            // 
+            this.text.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.text.HeaderText = "Text";
+            this.text.Name = "text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 456);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "STRG Editor By Angel333119";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn text;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
+
---
+++ STRGeditor/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+
+namespace STRGeditor
+{
+    public partial class Form1 : Form
+    {
+        string[][] textos;
+
+        uint bigendian(uint le)
+        {
+            uint pr = le >> 24;
+            uint se = le >> 8 & 0x00FF00;
+            uint te = le << 24;
+            uint qu = le << 8 & 0x00FF0000;
+            return pr | se | te | qu;
+        }
+
+        byte[] ntc;
+        uint magic;
+        uint version;
+        uint langnumb;
+        uint textnumb;
+        uint nametable;
+        uint nts;
+
+        string arquivo_aberto;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "DKCR or MP 3 STRG|*.STRG|All files (*.*)|*.*";
+            openFileDialog1.Title = "Select a STRG File";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                arquivo_aberto = openFileDialog1.FileName;
+
+                FileStream arq = new FileStream(openFileDialog1.FileName, FileMode.Open);
+
+                BinaryReader br = new BinaryReader(arq);
+
+                magic = bigendian(br.ReadUInt32());
+                version = bigendian(br.ReadUInt32());
+                langnumb = bigendian(br.ReadUInt32());
+                textnumb = bigendian(br.ReadUInt32());
+                nametable = bigendian(br.ReadUInt32());
+                nts = bigendian(br.ReadUInt32());
+                dataGridView1.Rows.Clear();
+                uint val = nts + 0x18;
+                ntc = br.ReadBytes((int)nts);
+
+                listBox1.Items.Clear();
+
+                arq.Seek(val, SeekOrigin.Begin);
+
+                for (int i = 0; i < langnumb; i++)
+                {
+
+                    byte[] btexto = br.ReadBytes(4);
+                    string texto = Encoding.ASCII.GetString(btexto);
+                    listBox1.Items.Add(texto);
+
+                }
+
+                textos = new string[langnumb][];
+
+                for (int i = 0; i < langnumb; i++)
+                {
+                    textos[i] = new string[textnumb];
+                    uint str_block_len = bigendian(br.ReadUInt32());
+
+                    for (int j = 0; j < textnumb; j++)
+                    {
+                        uint prpt = bigendian(br.ReadUInt32());
+
+                        long pos_antiga = arq.Position;
+
+                        uint primeirotexto = (4 + textnumb * 4) * langnumb + val + langnumb * 4;
+
+                        arq.Seek(primeirotexto + prpt, SeekOrigin.Begin);
+                        byte[] textg = br.ReadBytes((int)bigendian(br.ReadUInt32()) - 1);
+                        string textt = Encoding.UTF8.GetString(textg);
+
+                        arq.Seek(pos_antiga, SeekOrigin.Begin);
+
+                        dataGridView1.Rows.Clear();
+                        textos[i][j] = textt;
+                    }
+                }
+
+                arq.Dispose();
+            }
+
+            openFileDialog1.Dispose();
+        }
+
+        private void arquivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int old_index = -1;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex > -1)
+            {
+                salvar();
+
+                dataGridView1.Rows.Clear();
+
+                for (int i = 0; i < textos[listBox1.SelectedIndex].Length; i++)
+                {
+                    dataGridView1.Rows.Add(textos[listBox1.SelectedIndex][i]);
+                }
+
+                old_index = listBox1.SelectedIndex;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            salvar_arq(arquivo_aberto);
+        }
+
+        private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            salvar_arq_com_dialogo(false);
+        }
+
+        private void salvarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            salvar_arq_com_dialogo(true);
+        }
+
+        private void salvar_arq_com_dialogo(bool Comprimir)
+        {
+            SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+            SaveFileDialog1.Filter = "DKCR or MP 3 STRG|*.STRG|All files (*.*)|*.*";
+            SaveFileDialog1.Title = "Save a STRG File";
+
+            if (SaveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                salvar_arq(SaveFileDialog1.FileName, Comprimir);
+            }
+        }
+
+        private void salvar_arq(string FileName, bool Comprimir = false)
+        {
+            salvar();
+
+            FileStream arq = new FileStream(FileName, FileMode.Create);
+
+            BinaryWriter bw = new BinaryWriter(arq);
+
+            bw.Write(bigendian(magic));
+            bw.Write(bigendian(version));
+            bw.Write(bigendian(langnumb));
+            bw.Write(bigendian(textnumb));
+            bw.Write(bigendian(nametable));
+            bw.Write(bigendian(nts));
+            bw.Write(ntc);
+
+            for (int i = 0; i < langnumb; i++)
+            {
+                bw.Write(Encoding.ASCII.GetBytes(listBox1.Items[i].ToString()));
+            }
+
+            MemoryStream ms = new MemoryStream();
+            BinaryWriter bw2 = new BinaryWriter(ms);
+
+            for (int i = 0; i < textos.Length; i++)
+            {
+                uint tamanho_strings = 0;
+
+                for (int j = 0; j < textos[i].Length; j++)
+                {
+                    byte[] texto_utf8 = Encoding.UTF8.GetBytes(textos[i][j]);
+
+                    tamanho_strings += (uint)texto_utf8.Length + 1;
+                }
+
+                bw.Write(bigendian(tamanho_strings));
+
+                for (int j = 0; j < textos[i].Length; j++)
+                {
+                    bw.Write(bigendian((uint)ms.Position));
+                    byte[] texto_bytes = Encoding.UTF8.GetBytes(textos[i][j]);
+                    bw2.Write(bigendian((uint)texto_bytes.Length + 1));
+                    bw2.Write(texto_bytes);
+                    bw2.Write('\0');
+                }
+            }
+
+            bw.Write(ms.ToArray());
+            ms.Dispose();
+
+            arq.Dispose();
+
+            if (Comprimir) File.WriteAllBytes(FileName, Compress(File.ReadAllBytes(FileName)));
+        }
+        
+        private void salvar()
+        {
+            if (old_index > -1)
+            {
+
+                for (int i = 0; i < textos[0].Length; i++)
+                {
+                    string val = (string)dataGridView1.Rows[i].Cells[0].Value;
+
+                    if (val == null) val = string.Empty;
+
+                    textos[old_index][i] = val;
+                }
+            }
+        }
+
+
+        private byte[] Compress(byte[] Data)
+        {
+            using (MemoryStream Stream = new MemoryStream())
+            {
+                DeflateStream Compressor = new DeflateStream(Stream, CompressionLevel.Optimal);
+                Compressor.Write(Data, 0, Data.Length);
+                Compressor.Close();
+
+                using (MemoryStream NewStream = new MemoryStream())
+                {
+                    BinaryWriter Writer = new BinaryWriter(NewStream);
+                    Writer.Write((ushort)0xda78);
+                    Writer.Write(Stream.ToArray());
+                    Writer.Write(bigendian(Adler32(Data)));
+                    return NewStream.ToArray();
+                }
+            }
+        }
+
+        private uint Adler32(byte[] Data)
+        {
+            const int MOD_ADLER = 65521;
+
+            uint a = 1, b = 0;
+            for (int Index = 0; Index < Data.Length; Index++)
+            {
+                a = (a + Data[Index]) % MOD_ADLER;
+                b = (b + a) % MOD_ADLER;
+            }
+
+            return (b << 16) | a;
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("STRG editor created by Angel333119");
+        }
+    }
+}
